507e78a363b853912a7d944581f464399cb6f78a
trebloc <ctse77@alum.berkeley.edu>
2015-12-11 16:42:53 -0800

    established project with files and folders

---
+++ server.js
@@ -0,0 +1,32 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+var express = require('express');
+// generate a new express app and call it 'app'
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/************
+ * DATABASE *
+ ************/
+
+// var db = require('./models');
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(process.env.PORT || 3000)
