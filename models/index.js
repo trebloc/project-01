61b5499066b0dd341feedd8cf03a368ceb690aaa
trebloc <ctse77@alum.berkeley.edu>
2015-12-17 11:48:30 -0800

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/bikeshare_test");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                      process.env.MONGOHQ_URL || 
+                     "mongodb://localhost/bikeshare" )
 var Comment = require('./comments');
 var Station = require('./station');
 var Suggest = require('./suggest');
@@ -7,3 +9,5 @@ var Suggest = require('./suggest');
 module.exports.Comment = Comment;
 module.exports.Station = Station;
 module.exports.Suggest = Suggest;
+
+
